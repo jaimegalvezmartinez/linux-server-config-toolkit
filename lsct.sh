afde7310ba60d4c49917575322d6b52fd66a908a
Jaime Galvez Martinez <121205552+jaimegalvezmartinez@users.noreply.github.com>
2026-04-08 16:48:40 +0000

    Enable Podman socket if Docker is installed

--- lsct.sh
+++ lsct.sh
@@ -2360,6 +2360,7 @@ install_docker() {
           https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
         apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
         systemctl enable --now docker
+		systemctl enable --now podman.socket
     else
         echo "✅ Docker is already installed."
     fi
